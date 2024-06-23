8ec7502120764435bb7bddcecda3e66531c8b5b2
Mokhamed B <m.barhush@gmail.com>
2024-06-23 14:14:57 +0200

    добавлена форма

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,17 @@
 // isAvifWebp({ mode: 'webp' });
 
 // import Swiper bundle with all modules installed
-// import Swiper from "swiper/bundle";
+import Swiper from "swiper/bundle";
 // import styles bundle
-// import "swiper/css/bundle";
+import "swiper/css/bundle";
 
+import form from "./modules/form.js";
 
 window.addEventListener("load", function () {
   // Переменные
   const body = document.querySelector("body");
+
+  form();
 
   // Показываем страницу после загрузки
   body.style.opacity = 1;
